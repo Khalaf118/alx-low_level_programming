2e54cb673c2ae79adb2fdc55eef193f482fc80e0
Khalafawy <141347547+Khalaf118@users.noreply.github.com>
2023-09-08 11:04:17 -0700

    6. Numberz

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int digit;
+
+	for (digit = 48; digit < 58; digit++)
+		putchar(digit);
+	putchar(10);
+	return (0);
+}

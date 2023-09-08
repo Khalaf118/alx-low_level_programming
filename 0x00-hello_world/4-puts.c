62ef49abc9022a6448b633049205c75d0e0db40c
Khalafawy <141347547+Khalaf118@users.noreply.github.com>
2023-09-07 19:25:14 -0700

    4. Hello, puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,13 @@
 #include <stdio.h>
-
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
 	const char string[] = "\"Programming is like building a multilingual puzzle";
-	puts( string );
 
-	return(0);
+	puts(string);
+	return (0);
 }
-

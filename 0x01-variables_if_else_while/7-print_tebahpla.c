311b73ab7d95a660a739c050ab29d67492e073d8
Khalafawy <141347547+Khalaf118@users.noreply.github.com>
2023-09-08 11:09:25 -0700

    7. Smile in the mirror

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int ch;
+
+	ch = 122;
+	while (ch > 96)
+	{
+		putchar(ch);
+		ch--;
+	}
+	putchar(10);
+	return (0);
+}

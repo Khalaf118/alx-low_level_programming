4b7ea59ed54cc1dca9185187f1952ddc62882676
Khalafawy <141347547+Khalaf118@users.noreply.github.com>
2023-09-13 14:19:28 -0700

    Fibonacci

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int n;
+	long int t;
+	long int fc = 1;
+	long int fp = 1;
+
+	for (n = 1; n < 50; n++)
+	{
+		printf("%ld, ", fc);
+		t = fp + fc;
+		fp = fc;
+		fc = t;
+	}
+	printf("%ld\n", fc);
+	return (0);
+}

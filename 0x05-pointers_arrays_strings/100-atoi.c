b584b26ccf832b666e49aed8c62f4b9eb5c44383
Khalafawy <141347547+Khalaf118@users.noreply.github.com>
2023-09-19 13:41:50 -0700

    atoi

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -1,12 +1,30 @@
-#include <stdio.h>
 /**
  * _atoi - str to i.
  * @s: a pointer to the str.
- * 
+ *
  *
  * Return: the i
  */
 int _atoi(char *s)
 {
-	
+	int i = 0;
+	int num = 0;
+	int sign = 1;
+
+	while (s[i])
+	{
+		if (s[i] >= 48 && s[i] <= 57)
+		{
+			while (s[i] >= 48 && s[i] <= 57)
+			{
+				num = (num * 10) + s[i] - 48;
+				i++;
+			}
+			break;
+		}
+		if (s[i] == '-')
+			sign *= -1;
+		i++;
+	}
+	return (num * sign);
 }

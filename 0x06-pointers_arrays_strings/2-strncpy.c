e3c17784666543241b0b0415ca8c829a1afe3fad
Khalafawy <141347547+Khalaf118@users.noreply.github.com>
2023-09-21 08:28:26 -0700

    strcpy

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -9,17 +9,29 @@
  */
 char *_strncpy(char *dest, char *src, int n)
 {
-	int len1 = 0;
-	int len2 = 0;
+	int l1 = 0;
+	int l2 = 0;
 	int i;
 
-	while (dest[len1])
-		len1++;
-	while (src[len2])
-		len2++;
-	for (i = 0; i < n; i++)
-		dest[i] = src[i];
-	if (len1 <= len2 && n <= len2 && n >= len1)
-		dest[n] = '\0';
+	while (dest[l1])
+		l1++;
+	while (src[l2])
+		l2++;
+	if ((l1 >= l2 && l2 >= n) || (l1 <= l2 && l2 > n && l1 < n) || (l1 <= l2 && l2 > n && l1 <= n))
+	{
+		for (i = 0; i < n; i++)
+			dest[i] = src[i];
+		if (l1 <= l2 && l2 > n && l1 <= n)
+			dest[n] = '\0';
+	}
+	else
+	{
+		for (i = 0; i < l2; i++)
+			dest[i] = src[i];
+		for (i = l2; i < n; i++)
+			dest[i] = '\0';
+		if (n > l1)
+			dest[n] = '\0';
+	}
 	return (dest);
 }

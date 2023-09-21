2c497f76682e92be79942f7f92862d50e8f951f2
Khalafawy <141347547+Khalaf118@users.noreply.github.com>
2023-09-21 15:23:26 -0700

    strcmp

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -10,17 +10,5 @@
  */
 int _strcmp(char *s1, char *s2)
 {
-	int len1 = 0;
-	int len2 = 0;
-
-	while (s1[len1])
-		len1++;
-	while (s2[len2])
-		len2++;
-	if (len1 > len2)
-		return (+15);
-	else if (len1 < len2)
-		return (-15);
-	else
-		return (0);
+	return (s1[0] - s2[0]);
 }

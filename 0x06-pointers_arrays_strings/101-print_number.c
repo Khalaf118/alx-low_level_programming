0380a62796b8a74f6118b2566a60d5f94d2b7927
Khalafawy <141347547+Khalaf118@users.noreply.github.com>
2023-09-26 18:58:02 -0700

    leet_1337

---
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -0,0 +1,11 @@
+#include "main.h"
+
+/**
+ * print_number - prints an integer.
+ * @n: The integer to be printed.
+ *
+ * Return: void
+ */
+void print_number(int n)
+{
+}
--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -8,17 +8,23 @@
 char *leet(char *str)
 {
 	int i = 0;
+	int j;
+	char *s1 = "AaEeOoTtLl";
+	char *s2 = "4433007711";
 
 	while (str[i])
 	{
 		if (str[i] == 'A' || str[i] == 'a' ||
-			str[i] == 'E' || str[i] == 'e' ||
-			str[i] == 'O' || str[i] == 'o' ||
-			str[i] == 'T' || str[i] == 't' ||
-			str[i] == 'L' || str[i] == 'l')
-
-		if (str[i] >= 97 && str[i] <= 122)
-			str[i] -= 32;
+				str[i] == 'E' || str[i] == 'e' ||
+				str[i] == 'O' || str[i] == 'o' ||
+				str[i] == 'T' || str[i] == 't' ||
+				str[i] == 'L' || str[i] == 'l')
+		{
+			j = 0;
+			while (str[i] != s1[j])
+				j++;
+			str[i] = s2[j];
+		}
 		i++;
 	}
 	return (str);

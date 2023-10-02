31ba72de435eae8d04af40d3aad8618760fdf601
Khalafawy <141347547+Khalaf118@users.noreply.github.com>
2023-10-01 18:46:03 -0700

    palindrome

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -1,5 +1,4 @@
-#include "main.h"
-
+int _strlen_recursion(char *s);
 int _palindrome_(char *str, int len);
 /**
  * is_palindrome - a string is a palindrome or not.
@@ -36,3 +35,16 @@ int _palindrome_(char *str, int len)
 	else
 		return (1);
 }
+/**
+ * _strlen_recursion - string length.
+ * @s: The string.
+ *
+ * Return: The length
+ */
+int _strlen_recursion(char *s)
+{
+	if (s[0] == '\0')
+		return (0);
+	return (1 + _strlen_recursion(s + 1));
+}
+

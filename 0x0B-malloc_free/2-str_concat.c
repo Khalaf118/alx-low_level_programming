a2f657239bc3fb3c833f775f298c93e1adaddff5
Khalafawy <141347547+Khalaf118@users.noreply.github.com>
2023-10-04 15:34:08 -0700

    strcat

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -18,6 +18,10 @@ char *str_concat(char *s1, char *s2)
 	unsigned int i, n;
 	char *str;
 
+	if (s1 == NULL)
+		s1 = "";
+	if (s2 == NULL)
+		s2 = "";
 	while (s1[len1])
 		len1++;
 	while (s2[len2])

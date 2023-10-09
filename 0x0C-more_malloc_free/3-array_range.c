62c78b046816a8082979306a8fa5491bfcc3097b
Khalafawy <141347547+Khalaf118@users.noreply.github.com>
2023-10-08 18:47:30 -0700

    calloc

---
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -0,0 +1,13 @@
+#include <stdlib.h>
+
+/**
+ * array_arrang - creates an array of integers.
+ * @min: min value 
+ * @max: max value
+ *
+ * Return: a pointer to the array
+ *
+ */
+int *array_range(int min, int max)
+{
+}

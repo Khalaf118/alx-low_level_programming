8423822669b8139c78e61c6c7a16c91d92691621
Khalafawy <141347547+Khalaf118@users.noreply.github.com>
2023-10-10 18:37:01 -0700

    struct

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,19 @@
+#ifndef _DOG_H
+#define _DOG_H
+
+/**
+ * struct dog - my 1st struct
+ * @name: name
+ * @age: age
+ * @owner: owner
+ *
+ * Description: no need for more
+ */
+struct dog
+{
+	char *name;
+	float *age;
+	char *owner;
+};
+
+#endif

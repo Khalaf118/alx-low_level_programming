7e7fcd66802c9ab99bee3fab6232aee2e3a39299
Khalafawy <mohamedkhalaf11@gmail.com>
2023-10-25 08:39:29 -0700

    execute_before_main

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * startupFunction - excuted function before main
+ *
+ * Return: void
+ */
+__attribute__ ((constructor)) void startupFunction(void)
+{
+	printf("You're beat! and yet, you must allow,\n"
+			"I bore my house upon my back!\n");
+}
